ad634f7d8396b6ee6e87e167c4fb8e6e2dd19313
Nounours31 <code.fages@gmail.com>
2020-09-04 14:31:46 +0200

    Update BRIConfigs.php

--- nanie/server/Envt/BRIConfigs.php
+++ nanie/server/Envt/BRIConfigs.php
@@ -10,19 +10,11 @@ abstract class eConfigConnues
 class BRIConfigs {
     private const _currentConfig = eConfigConnues.Nanie;
 
-    private const _ajaxWSURL = [ 'http://localhost/nanie/server/WS/BRIWS.php', 'http://localhost:88/nanie/server/WS/BRIWS.php'];
-
-    private const _DBUser = [ 'root', 'root' ];
-    private const _DBPassword = [ '', '' ];
-    private const _DBBaseName = [ 'melanie_fages', 'melanie_fages' ];
-    private const _DBHost = [ '127.0.0.1', '127.0.0.1' ];
-    private const _DBPort = [ 3307, 3306 ];
-
-
-    public static function DBUser() { return self::_DBUser[self::_currentConfig]; }
-    public static function DBPassword() { return self::_DBPassword[self::_currentConfig]; }
-    public static function DBBaseName() { return self::_DBBaseName[self::_currentConfig]; }
-    public static function DBHost() { return self::_DBHost[self::_currentConfig]; }
-    public static function DBPort() { return self::_DBPort[self::_currentConfig]; }
+    public const _ajaxWSURL = [ 'http://localhost/nanie/server/WS/BRIWS.php', 'http://localhost:88/nanie/server/WS/BRIWS.php'];
+    public const _DBUser = [ 'root', 'root' ];
+    public const _DBPassword = [ '', '' ];
+    public const _DBBaseName = [ 'melanie_fages', 'melanie_fages' ];
+    public const _DBHost = [ '127.0.0.1', '127.0.0.1' ];
+    public const _DBPort = [ 3307, 3306 ];
 }
 ?>
